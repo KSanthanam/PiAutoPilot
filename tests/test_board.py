cac7c7542ff20888c888b3831dd88aad7791f594
KK Santhanam <kk.santhanam@finchain.ai>
2020-05-18 08:34:48 +1000

    Added test

---
+++ tests/test_board.py
@@ -0,0 +1,13 @@
+
+from AutoPilot.Board import Board
+import pytest
+import sys
+import fake_rpi
+import time
+
+
+class TestBoardh():
+    def test_dummy(self):
+        watch = Board()
+        assert 1 == 1
+   
