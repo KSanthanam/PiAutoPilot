ed54d3df4c2bdefb549b4b33cc0987715537d283
KK Santhanam <kk.santhanam@finchain.ai>
2020-05-18 08:24:12 +1000

    Initial

---
+++ setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("LICENSE", "r") as fh:
+    license = fh.read()
+
+setuptools.setup(
+    name="PiAutoPilot",
+    version="0.0.1",
+    author="KK Santhanam",
+    author_email="KK.Santhanam@gmail.com",
+    description="Autopilot for RaspberryPi connected to F7 Flight Controller",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    requires=['wheel'],
+    url="https://github.com/ksanthanam/PiAutoPilot",
+    packages=setuptools.find_packages(),
+    setup_requires=['setuptools','wheel'],
+    download_url='https://github.com/KSanthanam/PiAutoPilot/archive/v_01.tar.gz',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    license=license,
+    python_requires='>=3.6',
+    keywords=['RaspberryPi', 'Autopilot', 'F7', 'Betaflight']
+)
