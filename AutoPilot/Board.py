5b960c4ce03a1ccb7805db80f243b6cc6ed771a6
KK Santhanam <kk.santhanam@finchain.ai>
2020-05-18 19:42:07 +1000

    Cleaned up

--- AutoPilot/Board.py
+++
@@ -1,7 +0,0 @@
-import time
-
-class Board(object):
-  def __init__(self, **kwargs):
-    """ Board(port)
-    """
-    
--- AutoPilot/__init__.py
+++ AutoPilot/__init__.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-from AutoPilot.Board import Board
+from AutoPilot.MultiWii import MultiWii
